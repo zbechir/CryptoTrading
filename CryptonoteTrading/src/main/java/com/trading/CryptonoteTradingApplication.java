cc17375fefea2327d8bc6f103e7af9c1f5d38e18
bechir <bechir@bechir.laptop.local.sofrecom.tn>
2017-12-04 15:18:20 +0100

    Final Update

--- CryptonoteTrading/src/main/java/com/trading/CryptonoteTradingApplication.java
+++ CryptonoteTrading/src/main/java/com/trading/CryptonoteTradingApplication.java
@@ -3,8 +3,6 @@ package com.trading;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
